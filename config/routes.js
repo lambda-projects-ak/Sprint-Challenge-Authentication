ab6d7f9dbadc32f63d2719de8519758f93ec2109
Kingaa6 <Kingaa6@gmail.com>
2019-03-01 09:00:32 -0800

    Add token verification route

--- config/routes.js
+++ config/routes.js
@@ -7,7 +7,8 @@ const { authenticate } = require('../auth/authenticate');
 const users = require('../database/models/user-models');
 
 const secret =
-  process.env.SECRET || 'this is a backup secret if there is no .env secret.';
+  process.env.JWT_SECRET ||
+  'this is a backup secret if you cannot access a .env secret.';
 
 // generate token
 const generateToken = user => {
@@ -81,7 +82,7 @@ router.post('/login', (req, res) => {
 });
 
 // get users
-router.get('/users', (req, res) => {
+router.get('/users', authenticate, (req, res) => {
   users.get().then(users => res.status(200).json({ success: true, users }));
 });
 
